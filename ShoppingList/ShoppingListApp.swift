704a93f0f49683329ab7f6c63c75178bd80e3e27
Joshua Hardy <thejdih@gmail.com>
2025-06-18 17:03:10 -0500

    Initial Commit

---
+++ ShoppingList/ShoppingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListApp.swift
+//  ShoppingList
+//
+//  Created by Joshua Hardy on 6/18/25.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
